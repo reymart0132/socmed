3f72455b622bac6c264bfcf50fe2a7767c21237b
reymart0132 <reymart.bolasoc@gmail.com>
2023-04-28 11:19:43 +0800

    login_page_code
    new loginpage

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
-require_once $_SERVER['DOCUMENT_ROOT'].'/socmed/resource/php/class/core/init.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/socialmedia/resource/php/class/core/init.php';
 
  ?>
 
@@ -16,12 +16,10 @@ require_once $_SERVER['DOCUMENT_ROOT'].'/socmed/resource/php/class/core/init.php
 <body>
         <nav class="navbar navbar-dark bg-white shadow-sm slide-in-left">
           <a class="navbar-brand " href="https://malolos.ceu.edu.ph/">
-            <img src="resource/img/logo.jpg" height="70" class="d-inline-block align-top"
+            <img src="resource/img/header.png" height="70" class="d-inline-block align-top"
               alt="mdb logo"><h3 class="ib">
           </a>
-             <a href="https:/www.facebook.com/theCEUofficial/"><i class="fab fa-facebook-f ceucolor"></i></a>
-             <a href="https://www.instagram.com/ceuofficial/"><i class="fab fa-instagram ceucolor"></i></a>
-             <a href="https://twitter.com/ceumalolos"><i class="fab fa-twitter ceucolor"></i></a>
+             <h6 class="text-primary"><b>Connect. Friends. Leisure.</b></h6>
         </nav>
            <div class="container mt-5">
             <div class="row justify-content-center">
@@ -30,25 +28,25 @@ require_once $_SERVER['DOCUMENT_ROOT'].'/socmed/resource/php/class/core/init.php
 
 
                     <form class="text-center border border-light p-5 shadow puff-in-center" action="" method="post" >
-                    <p class="h4 mb-4">Sign in</p>
+                    <p class="h4 mb-4 text-primary"><img src="resource/img/header2.png" width="auto" height="75" alt="">Sign in</p>
                     <?php logd();?>
                     <input type="text" id="username" class="form-control mb-4" name="username" placeholder="Enter Username" required>
                     <input type="password" id="defaultLoginFormPassword" class="form-control mb-4" placeholder="Enter Password" name ="password" required>
                     <div class="d-flex justify-content-around">
                     </div>
                     <input type =hidden name="token" value="<?php echo Token::generate(); ?>">
-                    <input  type="submit"  class="btn btn-dark btn-block my-4"value="Login"/>
+                    <input  type="submit"  class="btn btn-primary btn-block my-4"value="Login"/>
                     </form>
 
 
-                    <footer id="sticky-footer" class="py-4 bg-dark text-white-50 fixed-bottom slide-in-right">
+                    <footer id="sticky-footer" class="py-4 bg-primary text-white fixed-bottom slide-in-right">
                       <div class="container text-center">
                           <div class="row">
                               <div class="col col-sm-5 text-left">
                                   <small>Copyright &copy;Centro Escolar University     Office of the Registrar 2019</small>
                               </div>
                               <div class="col text-right">
-                                  <small>Created by: Reymart Bolasoc, Amelia Valencia , James Mangalile, Kenneth De Leon , Pamela Reyes , Ellen Mijares</small>
+                                  <small>Created by: Reymart Bolasoc</small>
                               </div>
                           </div>
                       </div>
